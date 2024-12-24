38fd3689c6fb31551fdc14e7b825c5cb33c15e58
diegoperezm <diegoperezcorreo@gmail.com>
2024-12-24 19:58:34 -0300

    Agregar test: createClient_Null

--- src/test/java/org/globant/talentodigital/telefonica/service/ClientServiceTest.java
+++ src/test/java/org/globant/talentodigital/telefonica/service/ClientServiceTest.java
@@ -28,21 +28,32 @@ public class ClientServiceTest {
 
     @BeforeEach
     public void setUp() {
-    client_1 = new Client();
-    client_1.setId(1L);
+        client_1 = new Client();
+        client_1.setId(1L);
         client_1.setFirstName("Juan");
         client_1.setLastName("Perez");
         client_1.setRun("123456789-9");
+
+        client_2 = new Client();
+        client_2.setId(2L);
+        client_2.setFirstName("Ramon");
+        client_2.setLastName("Estevez");
+        client_2.setRun("133456789-9");
     }
 
     @Test
-    void testCreateClient_NotNull() {
+    void createClient_Null() {
+        when(clientRepository.save(null)).thenReturn(null);
+        Client result = clientService.createClient(null);
+        assertNull(result);
+    }
+
+    @Test
+    void createClient_NotNull() {
         when(clientRepository.save(any(Client.class))).thenReturn(client_1);
         Client result = clientService.createClient(client_1);
         assertNotNull(result);
-   }
-
-
+    }
 
 }
 
